889b5336508b90f4e6d3053b2321e21591cae451
Erick santos <usernow.82@gmail.com>
2021-06-23 15:04:37 -0500

    Actualizando ejercicio

--- criptadoDetextoMatriz.py
+++ criptadoDetextoMatriz.py
@@ -1,11 +1,31 @@
+#El metodo bifido es un cifrado fraccionario,el metodo comienza con una matriz 5x5
+#palabra clave de cifrado "qwerty"
+#  1 2 3 4 5
+#1 q w e r t
+#2 y a b c d
+#3 f g h ij k
+#4 l m n o p
+#5 s u v x z
+#matriz de 5x5 i yj comparten lugares
+#las primeras posiciones son ocupadas por los valores de la clave
+#cifrar palabra "informatica"
+#34 43 31 44 14 42 22 15 34 24 22 ( fila, columna)
+#el equivalente numerico se divide en dos de la siguiente forma:
+#34433144144
+#22215342422
+#se combina nuevamente intercalando un digito de la primera mitad con la segunda mitad
+#32 42 42 31 35 13 44 42 14 42 42
+# volviendo a consultar la matriz de cifrado
+#gmmfkeomrmm ( palabra cifrada)
 import string
 def crearMatriz(palabra):
 
-    listaAbc = list(string.ascii_lowercase)
-    listaClave = list(palabra)
+    listaAbc = list(string.ascii_lowercase) #lista con el abc
+    listaClave = list(palabra) #lista con la clave
 
     for caracClave in listaClave: # remueve los caracteres dados por el parametro
-        listaAbc.remove(caracClave)
+        if caracClave in listaAbc:
+            listaAbc.remove(caracClave)
 
     total = listaClave + listaAbc
 
@@ -44,12 +64,13 @@ def coordenadaletra(coorF,CoordC, matriz):
 def cifrar (matriz,palabra): 
     eqNumerico = ""
     for letra in palabra:
-        coordenada = letraCoordenada(letra,matriz) #se ejeuta otra funcion dentro
-        eqNumerico += coordenada# que recibe la letra y la matriz
+        coordenada = letraCoordenada(letra,matriz) #se ejeuta una funcion para hallar la coordenada de la letra
+        eqNumerico += coordenada
     #print(eqNumerico)
-    mitad = len(eqNumerico)//2
+    mitad = len(eqNumerico)//2 
     mitadIzq = eqNumerico[:mitad]
     mitadDere = eqNumerico[mitad:]
+    
     eqNumericoIntercalado = []
     for i in range(len(mitadIzq)):#recorrido paralelo de las dos mitades
         digitoIzq = mitadIzq[i]
@@ -70,3 +91,11 @@ def cifrar (matriz,palabra):
 matriz = crearMatriz('qwerty')
 palabraCifrada = cifrar(matriz, 'informatica')
 print(palabraCifrada)
+
+#[ 1   2  3  4  5
+#1 [q, w ,e ,r, t]
+#2 [y, a, b, c, d]
+#3 [f, g, h ,ij, k]
+#4 [l, m, n, o, p]
+#5 [s, u, v, x, z ]
+#]
